eb1e7800d9f391daeacf27bac8ac7eb4430e78f4
Brock <147293696+Brock405@users.noreply.github.com>
2024-05-09 10:56:21 +1000

    Added gui
    NOTE doesn't work well NOT Stable

--- src/main.py
+++ src/main.py
@@ -1,10 +1,26 @@
 import keyboard
+import customtkinter
 
-howManyKeysArePressed = 0
-
-while True:
+def detectsIfButtonPressedDown():
+    howManyKeysArePressed = 0
     # detects if key pressed down
     keyEvent = keyboard.read_event()
-    if keyEvent.event_type == keyboard.KEY_DOWN:
+    if keyEvent.event_type == keyboard.KEY_UP:
         howManyKeysArePressed += 1
-        print(howManyKeysArePressed)
+        print(howManyKeysArePressed) 
+
+    return howManyKeysArePressed   
+
+
+# ---------------------------gui-----------------------------
+
+customtkinter.set_appearance_mode("dark")  # Modes: system (default), light, dark
+customtkinter.set_default_color_theme("blue")  # Themes: blue (default), dark-blue, 
+app = customtkinter.CTk()  # create CTk window like you do with the Tk window
+app.geometry("1280x720")
+
+wordsTypedTodayCounter = customtkinter.CTkLabel(app, text="asdf", fg_color="transparent")
+wordsTypedTodayCounter.place(relx=0.5, rely=0.5, anchor=customtkinter.CENTER)
+
+app.mainloop()
+
