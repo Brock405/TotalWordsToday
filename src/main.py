81fa62d608c172a76aa1dd5d09e562ce40175c4a
Brock <147293696+Brock405@users.noreply.github.com>
2024-05-10 15:20:22 +1000

    Put code into a class

--- src/main.py
+++ src/main.py
@@ -1,26 +1,39 @@
 import keyboard
 import customtkinter
 
-def detectsIfButtonPressedDown():
-    howManyKeysArePressed = 0
-    # detects if key pressed down
-    keyEvent = keyboard.read_event()
-    if keyEvent.event_type == keyboard.KEY_UP:
-        howManyKeysArePressed += 1
-        print(howManyKeysArePressed) 
 
-    return howManyKeysArePressed   
 
 
 # ---------------------------gui-----------------------------
 
-customtkinter.set_appearance_mode("dark")  # Modes: system (default), light, dark
-customtkinter.set_default_color_theme("blue")  # Themes: blue (default), dark-blue, 
-app = customtkinter.CTk()  # create CTk window like you do with the Tk window
-app.geometry("1280x720")
 
-wordsTypedTodayCounter = customtkinter.CTkLabel(app, text="asdf", fg_color="transparent")
-wordsTypedTodayCounter.place(relx=0.5, rely=0.5, anchor=customtkinter.CENTER)
 
+class App(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
+        self.geometry("1280x720")
+        self.title("CTk example")
+
+        self.howManyKeysArePressed = 0
+        self.detectsIfButtonPressedDown()
+
+        # add widgets to app
+        self.wordsTypedTodayCounter = customtkinter.CTkLabel(self, text=str(self.howManyKeysArePressed), fg_color="transparent")
+        self.wordsTypedTodayCounter.place(relx=0.5, rely=0.5, anchor=customtkinter.CENTER)
+        
+
+    # add methods to app
+    def detectsIfButtonPressedDown(self):
+        # detects if key pressed down
+        keyEvent = keyboard.read_event()
+        if keyEvent.event_type == keyboard.KEY_UP:
+            self.howManyKeysArePressed += 1
+            print(self.howManyKeysArePressed) 
+
+
+
+
+
+
+app = App()
 app.mainloop()
-
