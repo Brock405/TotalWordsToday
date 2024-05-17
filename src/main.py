ea73bfcacde4b075961b7d726e0b31c536c620ad
Brock <147293696+Brock405@users.noreply.github.com>
2024-05-17 11:15:58 +1000

    Working
    Added threads to make the gui and the counter run side by side. The gui updates every 100 ms. Working build finally. IT IS NOT DONE THOUGH

--- src/main.py
+++ src/main.py
@@ -1,39 +1,39 @@
-import keyboard
 import customtkinter
+import keyboard
+import threading
 
+howManyKeysArePressed = 0
 
+def detectsIfButtonPressedDown():
+    global howManyKeysArePressed
+    while True:
+        # detects if key pressed down
+        keyEvent = keyboard.read_event()
+        if keyEvent.event_type == keyboard.KEY_UP:
+            howManyKeysArePressed += 1
+            print(howManyKeysArePressed)
 
-
-# ---------------------------gui-----------------------------
-
+threadToCheckIfButtonPressed = threading.Thread(target=detectsIfButtonPressedDown)
+threadToCheckIfButtonPressed.start()
 
 
 class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
-        self.geometry("1280x720")
-        self.title("CTk example")
-
-        self.howManyKeysArePressed = 0
-        self.detectsIfButtonPressedDown()
+        self.geometry("960x720")
+        self.title("Total Words Today")
 
         # add widgets to app
-        self.wordsTypedTodayCounter = customtkinter.CTkLabel(self, text=str(self.howManyKeysArePressed), fg_color="transparent")
+        self.wordsTypedTodayCounter = customtkinter.CTkLabel(self, text=str(howManyKeysArePressed), fg_color="transparent")
         self.wordsTypedTodayCounter.place(relx=0.5, rely=0.5, anchor=customtkinter.CENTER)
-        
 
     # add methods to app
-    def detectsIfButtonPressedDown(self):
-        # detects if key pressed down
-        keyEvent = keyboard.read_event()
-        if keyEvent.event_type == keyboard.KEY_UP:
-            self.howManyKeysArePressed += 1
-            print(self.howManyKeysArePressed) 
-
-
-
+    def updateCharacterCounter(self):
+        self.wordsTypedTodayCounter.configure(text = str(howManyKeysArePressed))
+        self.after(100, self.updateCharacterCounter)
 
 
 
 app = App()
+app.after(100, app.updateCharacterCounter)
 app.mainloop()
