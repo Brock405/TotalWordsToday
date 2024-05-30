4d7f808248e53b670358a3c335e57ef76f994557
Brock <147293696+Brock405@users.noreply.github.com>
2024-05-30 10:54:42 +1000

    Added a start info screen
    Added a screen that tells you how to use the app.

--- src/main.py
+++ src/main.py
@@ -29,6 +29,7 @@ class App(customtkinter.CTk):
         super().__init__()
         self.geometry("580x420")
         self.title("Total Words Today") 
+        self.resizable(False, False)
 
         #  ------ add widgets to app ------
 
@@ -44,11 +45,28 @@ class App(customtkinter.CTk):
         self.wordsTypedTodayCounter = customtkinter.CTkLabel(self, font=('Roboto', 30, 'bold'), text= str(howManyWordsAreTyped), fg_color="transparent")
         self.wordsTypedTodayCounter.place(relx=0.8, rely=0.2,  anchor=customtkinter.CENTER)
 
+        # intro screen
+        self.introScreenBackground = customtkinter.CTkFrame(master=self, width=580, height=420)
+        self.introScreenBackground.place(relx=0.0, rely=0.0)
+
+        self.titleScreen = customtkinter.CTkLabel(self.introScreenBackground, text="Welcome to TWT", fg_color="transparent", bg_color="transparent", font=('Roboto', 42, 'bold'), corner_radius=10)
+        self.titleScreen.place(relx=0.5, rely=0.1, anchor=customtkinter.CENTER)
+
+        self.appInfoText = customtkinter.CTkLabel(self.introScreenBackground, text="This an app that will record various statistics about\n your keyboard and will send you a notification\n at the end of the day about your productivity.\n \n \n Press the button below to start recording todays stats", fg_color="transparent", bg_color="transparent", font=('Roboto', 16), corner_radius=10)
+        self.appInfoText.place(relx=0.5, rely=0.4, anchor=customtkinter.CENTER)
+
+        self.dismissInfoScreen = customtkinter.CTkButton(self.introScreenBackground, text="Start", command=self.startButtonEvent)
+        self.dismissInfoScreen.place(relx=0.5, rely=0.8, anchor=customtkinter.CENTER)
+
     # add methods to app  
     def updateCounter(self):
         self.charactersTypedTodayCounter.configure(text = str(howManyKeysArePressed))
         self.wordsTypedTodayCounter.configure(text = str(howManyWordsAreTyped))
         self.after(100, self.updateCounter)
+
+    def startButtonEvent(self):
+        self.introScreenBackground.destroy()
+
 
 app = App()
 app.after(100, app.updateCounter)
